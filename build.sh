31d860a4aef4b2bb237e8be3d6e705696209bbc4
kpleiner <klemens.pleiner@systemtera.com>
2017-03-04 08:38:30 +0100

    STA-2544: ServerV kann eth0 nicht verwenden Changing uboot to have it look like uboot suggested by Variscite for Linux Kernel 3.14 (http://variwiki.com/index.php?title=VAR-SOM-AM33_Yocto_Daisy_GS => git://github.com/varigit/u-boot-VAR-SOM-AM33-SDK7.git)

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-echo "Building u-boot with var-som-am33 config..."
-make distclean
-make var-som-am33-sd
+#echo "Building u-boot with var-som-am33 config..."
+#make distclean
+#make var-som-am33-sd
+#
+echo "Building u-boot as mentioned in http://variwiki.com/index.php?title=VAR-SOM-AM33_Yocto_Krogoth_Build_u-boot_out_of_tree#Building_U-boot"
+echo "U-boot variant to boot from NAND flash:"
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- var-som-am33
+#
+#echo "U-boot variant to boot from SD-Card:"
+#make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- var-som-am33-sd
